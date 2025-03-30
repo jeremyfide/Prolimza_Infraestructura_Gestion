ce616fb4bc27fad5670025c03dee7107f0d65bda
jeremyfide <jramirez20454@ufide.ac.cr>
2025-03-30 15:26:22 -0600

    Delete Prolimza directory

--- Prolimza/Prolimza/Models/Provincia.cs
+++
@@ -1,9 +0,0 @@
-namespace Prolimza.Models
-{
-    public class Provincia
-    {
-        public int IdProvincia { get; set; }
-        public string Nombre { get; set; }
-        public ICollection<Canton> Cantones { get; set; }
-    }
-}
