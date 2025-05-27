7f52ca061da1e665e17f821a739c031ffcea1058
jbarrant22 <jbarrantes50645@ufide.ac.cr>
2025-05-26 20:22:47 -0600

    Inclusion auditoria, materia prima, estado de venta e historial estado de venta

---
+++ Prolimza/Prolimza/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prolimza.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Prolimza.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Prolimza.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string correo, string contrasena)
+        {
+            var usuario = _context.Usuarios.Include(u => u.Rol).FirstOrDefault(u => u.Correo == correo);
+
+            if (usuario == null || !PasswordHelper.VerifyPassword(contrasena, usuario.ContrasenaEncriptada))
+            {
+                ViewBag.Error = "Correo o contraseña incorrectos.";
+                return View();
+            }
+
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, usuario.Nombre),
+            new Claim(ClaimTypes.Email, usuario.Correo),
+            new Claim(ClaimTypes.Role, usuario.Rol.Nombre),
+            new Claim("IdUsuario", usuario.IdUsuario.ToString())
+        };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+    }
+}
