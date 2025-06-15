3c6e46b385f61012fbb53b79a5b93bcecc356ec1
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-06-15 03:05:31 -0600

    Cambios UI. Side bar, Data tables, Home, Login.

--- Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
+++ Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e743f7024617fa3a1da512f4c1545502c334b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ca25460bc91228a84010dd74bd56c5b42b841d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
