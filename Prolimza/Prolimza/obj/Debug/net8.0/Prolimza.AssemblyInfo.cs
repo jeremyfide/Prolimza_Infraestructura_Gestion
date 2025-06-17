d0c4d94b2ea01cd378dd9167d2e8cc1a4aca1c74
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-06-16 20:41:45 -0600

    Cambios UI. Roles.

--- Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
+++ Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6e46b385f61012fbb53b79a5b93bcecc356ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95cc531541ecff10003c34bbda461ed177eafa4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
