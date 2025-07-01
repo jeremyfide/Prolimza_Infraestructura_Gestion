3df2e7d3a4fe87a7a4801406c155273a4e9b89fe
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-06-30 19:09:45 -0600

    Correccion modelo compras.

--- Prolimza/Prolimza/Models/Compra.cs
+++ Prolimza/Prolimza/Models/Compra.cs
@@ -10,8 +10,8 @@ namespace Prolimza.Models
 
         public int IdUsuario { get; set; }
 
-        //[Required(ErrorMessage = "El campo Código de Ingreso es obligatorio.")]
-        public string? CodigoIngreso { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El campo Código de Ingreso es obligatorio.")]
+        public string CodigoIngreso { get; set; }
 
         public string Estado { get; set; } = "Pendiente";
 
