71c61dab0182c440c1113e2c584cb4e31cb99f8f
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-07-13 23:29:01 -0600

    Implementacion inicial de AI.

---
+++ Prolimza/Prolimza/Controllers/PrediccionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Prolimza.Services.IA;
+using Prolimza.Models;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prolimza.Controllers
+{
+    public class PrediccionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrediccionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Productos.ToList();
+            ViewBag.Productos = new SelectList(productos, "IdProducto", "NombreProducto");
+
+            return View(0); // Valor por defecto del idProducto
+        }
+
+        [HttpPost]
+        public IActionResult Predecir(int idProducto)
+        {
+            var servicio = new PrediccionService(_context);
+            var resultados = servicio.PredecirVentas(idProducto);
+            var producto = _context.Productos.FirstOrDefault(p => p.IdProducto == idProducto)?.NombreProducto;
+
+            ViewBag.Producto = producto;
+            ViewBag.Resultados = resultados;
+            ViewBag.Productos = new SelectList(_context.Productos.ToList(), "IdProducto", "NombreProducto", idProducto);
+
+            return View("Index", idProducto);
+        }
+
+    }
+}
--- Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
+++ Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ad4abe70d16434d7a2a9a328497ac2198a891b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83032add5542b5e01d5e6dc04dbbb44d76878c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
