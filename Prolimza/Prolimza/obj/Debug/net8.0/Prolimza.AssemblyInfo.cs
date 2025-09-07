4bee0c884233e1795264aeb7550bae1a6927ed1d
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-09-07 16:42:00 -0600

    Azure DB

--- Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
+++ Prolimza/Prolimza/obj/Debug/net8.0/Prolimza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f7459f98fd6422922dc391c23db4826c110c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052d2f32b645a14a3a77bc4172cd785394c3d945")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prolimza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
