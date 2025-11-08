5f52513752f0690d6008af0bb5aca534b0ae4769
JeremyRamirezG <87102105+JeremyRamirezG@users.noreply.github.com>
2025-11-08 13:54:39 -0600

    deployAzure

--- Prolimza/Prolimza/Controllers/AlertasController.cs
+++ Prolimza/Prolimza/Controllers/AlertasController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿//Test deployment
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
